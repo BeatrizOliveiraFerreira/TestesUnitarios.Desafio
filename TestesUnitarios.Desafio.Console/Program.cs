3120d5822f972ae3b2878515b7faf4902a76b827
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2024-04-04 22:10:38 -0300

    Adicionar arquivos de projeto.

---
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using TrilhaTarefas;
+
+namespace TrilhaTarefasTests
+{
+    public class ValidacoesListaTests
+    {
+        [Fact]
+        public void DeveRemoverNumerosNegativosDeUmaLista()
+        {
+            // Arrange
+            var lista = new int[] { -1, 2, -3, 4, -5 };
+
+            // Act
+            var resultado = ValidacoesLista.RemoverNumerosNegativos(lista);
+
+            // Assert
+            Assert.All(resultado, num => Assert.True(num > 0));
+        }
+
+        // Implemente os demais métodos de teste da classe ValidacoesListaTests
+    }
+
+    public class ValidacoesStringTests
+    {
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            // Arrange
+            var texto = "Matrix";
+
+            // Act
+            var resultado = ValidacoesString.RetornarQuantidadeCaracteres(texto);
+
+            // Assert
+            Assert.Equal(6, resultado);
+        }
+
+        // Implemente os demais métodos de teste da classe ValidacoesStringTests
+    }
+}
+
